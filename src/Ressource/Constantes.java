1372e692eab8dd06371f004be87f7affc5c43c53
sundaday <john.koch.pro@gmail.com>
2020-11-27 22:08:10 +0100

    first commit

---
+++ src/Ressource/Constantes.java
@@ -0,0 +1,9 @@
+package Ressource;
+
+public abstract class Constantes {
+
+    //Dimension fenetre
+    public static final int LARGUEUR_FENETRE = 600;
+    public static final int HAUTEUR_FENETRE = 600;
+    public static final int MARGE_FENETRE = 600;
+}
---
+++ src/com/sundaday/jeu/Main.java
@@ -0,0 +1,11 @@
+package com.sundaday.jeu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Creation de la fenetre de l'application
+
+
+
+    }
+}
